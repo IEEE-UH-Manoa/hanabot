3b535186d3517fce9cfead32ac4a7ffb6fdb1924
Kenny Luong <luong97@hawaii.edu>
2015-03-06 18:03:52 -1000

    Added tasks

---
+++ scripts/tasks.js
@@ -0,0 +1,44 @@
+var http = require('http');
+module.exports = function(robot) {
+    robot.respond(/events/i, function(msg){
+        //The url we want is: 'www.random.org/integers/?num=1&min=1&max=10&col=1&base=10&format=plain&rnd=new'
+        var options = {
+          host: 'api.ieeeatuhm.com',
+          path: '/tasks'
+        };
+
+        callback = function(response) {
+          var str = '';
+
+          //another chunk of data has been recieved, so append it to `str`
+          response.on('data', function (chunk) {
+            str += chunk;
+          });
+
+          //the whole response has been recieved, so we just print it out here
+          response.on('end', function () {
+            parse_tasks(str, function(output){
+                msg.send(output);
+            });
+          });
+        }
+
+        http.request(options, callback).end();
+    });
+}
+
+
+function parse_tasks(response, callback){
+    var output = ' ';
+    var data = JSON.parse(response);
+
+    var output = data.join("\n");
+        
+
+    output = "\n" 
+        + "*Officers be slacking:* \n" 
+        + "----------------------\n"
+        + output;
+
+    callback(output);
+}
