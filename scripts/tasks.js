a8a68ad8fbbf41433f883f55753e9f14a0bccfa5
Kenny Luong <luong97@hawaii.edu>
2015-03-06 18:05:11 -1000

    Edited tasks handler

--- scripts/tasks.js
+++ scripts/tasks.js
@@ -1,6 +1,6 @@
 var http = require('http');
 module.exports = function(robot) {
-    robot.respond(/events/i, function(msg){
+    robot.respond(/tasks/i, function(msg){
         //The url we want is: 'www.random.org/integers/?num=1&min=1&max=10&col=1&base=10&format=plain&rnd=new'
         var options = {
           host: 'api.ieeeatuhm.com',
